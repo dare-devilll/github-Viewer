1eeaefcfac31b7f33839cae1f0f644c7fa35fbf3
vedant dandge <vedantdandgepro@gmail.com>
2022-07-20 20:12:09 +0530

    first commit

---
+++ gitBrain.js
@@ -0,0 +1,80 @@
+const APIURL = "https://api.github.com/users/";
+
+const main = document.getElementById("main");
+const form = document.getElementById("form");
+const search = document.getElementById("search");
+
+getUser("dare-devilll");
+async function getUser(username) {
+    
+    const resp = await fetch(APIURL + username);
+    const respData = await resp.json();
+    console.log(respData);
+    createUserCard(respData);
+
+    getRepos(username);
+}
+
+async function getRepos(username) {
+    const resp = await fetch(APIURL + username + "/repos");
+    const respData = await resp.json();
+
+    addReposToCard(respData);
+}
+
+function createUserCard(user) {
+    const cardHTML = `
+        <div class="card">
+            <div>
+                <img class="avatar" src="${user.avatar_url}" alt="${user.name}" />
+            </div>
+            <div class="user-info">
+            <a href="${user.html_url}"><h2>${user.name}</h2></a>
+                <p>${user.bio}</p>
+                <a href="${user.blog}">${user.blog}</a>
+
+                <ul class="info">
+                    <li>${user.followers}<strong>Followers</strong></li>
+                    <li>${user.following}<strong>Following</strong></li>
+                    <li>${user.public_repos}<strong>Repos</strong></li>
+                </ul>
+
+                <div id="repos"></div>
+            </div>
+        </div>
+    `;
+
+    main.innerHTML = cardHTML;
+}
+
+function addReposToCard(repos) {
+    const reposEl = document.getElementById("repos");
+
+    repos
+        .sort((a, b) => b.stargazers_count - a.stargazers_count)
+        .slice(0, Infinity)
+        .forEach((repo) => {
+            const repoEl = document.createElement("a");
+            repoEl.classList.add("repo");
+
+            repoEl.href = repo.html_url;
+            repoEl.target = "_blank";
+            repoEl.innerText = repo.name;
+
+            reposEl.appendChild(repoEl);
+        });
+}
+
+// starts here.
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const user = search.value;
+
+    if (user) {
+        getUser(user);
+
+        search.value = "";
+    }
+});
+
